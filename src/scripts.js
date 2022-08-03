4ab4bb70986758fcf97b57ecc165bd5abfda053b
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-08-02 22:06:30 -0300

    add um service work ao projeto

---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+// Service Worker
+
+if('serviceWorker' in navigator){
+    window.addEventListener('load', function(){
+        navigator.serviceWorker.register('./src/sw.js').then(function(registration){
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err){
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
---
+++ src/sw.js
@@ -0,0 +1,46 @@
+var myCacheName = 'comoserumamanicure-cache-v1';
+
+//Adding `install` event listener
+window.self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('myCacheName')
+      .then((cache) => {
+        return cache.addAll([
+          './',
+          './public/index.html',
+          './public/favicon_io/',
+          './public/sitemap_index.xml',
+          './src/imgages/',
+          './src/sw.js',
+          './src/App.css',
+          './src/App.js',
+          './src/index.js'
+        ]);
+      })
+    );
+  });
+
+  window.self.addEventListener('activate', (event) => {
+  const cacheWhiteList = ['pwa-task-manager'];
+  event.waitUntil(caches.keys()
+    .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => {
+      if (cacheWhiteList.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+      return null
+    })))
+  )
+});
+
+window.self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.open(myCacheName)
+    .then((cache) => cache.match(event.request)
+      .then((response) => response || fetch(event.request)
+        .then((res) => {
+          cache.put(event.request, res.clone());
+          return res;
+        })
+      )
+    )
+  )
+});
